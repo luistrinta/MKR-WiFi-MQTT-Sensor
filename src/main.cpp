0612a32ac7d7cfd63ef709daaac0d32421fc4c01
Luis Trinta <33879012+luistrinta@users.noreply.github.com>
2024-08-05 18:24:48 +0100

    Tweaked client library

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,20 @@ MKRIoTCarrier carrier;
 WiFiClient wifiClient;
 PubSubClient client(wifiClient);
 
+float temperature = 0;
+float humidity = 0;
+float moisture = 0;
+const int moistPin = A6;
+
+void resetScreen()
+{
+  carrier.display.setTextSize(2);
+  carrier.display.setCursor(20, 100);
+}
+
 void connectWiFi(){
   while(WiFi.status() != WL_CONNECTED){
+    resetScreen();
     Serial.println("Connecting to WiFi...");
     carrier.display.fillScreen(0xFC00);
     carrier.display.print("Connecting to WiFi...");   
@@ -19,8 +31,9 @@ void connectWiFi(){
   }
 
   Serial.println("Connected!");
-  carrier.display.fillScreen(0xFC00);
+  carrier.display.fillScreen(0x07E0);
   carrier.display.print("Connected");
+  delay(2000);
 
 }
 
@@ -28,6 +41,7 @@ void reconnectToMQTTClient()
 {
   while(!client.connected())
   {
+    resetScreen();
     Serial.println("Attempting to reconnect to MQTTClient");
     carrier.display.fillScreen(0xFC00);
     carrier.display.print("Attempting to reconnect to MQTTClient");
@@ -35,18 +49,20 @@ void reconnectToMQTTClient()
     if(client.connect(CLIENT_NAME))
     {
       Serial.println("Connected");
-      carrier.display.fillScreen(0xFC00);
+      carrier.display.fillScreen(0x07E0);
       carrier.display.print("Connected");
+      delay(2000);
     }
     else
     {
       Serial.println("Retrying in 5 seconds - failed, rc =");
       Serial.println(client.state());
-      carrier.display.fillScreen(0xFC00);
+      carrier.display.fillScreen(0xF800);
       carrier.display.println("Retrying in 5 seconds - failed, rc =");
       carrier.display.println(client.state());
 
-      delay(5000);
+    randomSeed(analogRead(A0));
+    delay(random(2000, 5000));
     }
   }
 }
@@ -54,11 +70,9 @@ void reconnectToMQTTClient()
 void createMQTTClient()
 {
   client.setServer(BROKER,PORT_NUMBER);
-  client.setKeepAlive(60);
   reconnectToMQTTClient();
+  client.setKeepAlive(30);
 }
-
-
 
 void setup() {
   Serial.begin(9600);
@@ -66,29 +80,50 @@ void setup() {
   carrier.begin();
   connectWiFi();
   createMQTTClient();
-  carrier.display.fillScreen(0xFC00);
-  carrier.display.setTextColor(0x0000);
-  carrier.display.setTextSize(3);
-  carrier.display.setCursor(20, 20);
+  resetScreen();
 }
 
 void loop() {
+  resetScreen();
   reconnectToMQTTClient();
   client.loop();
+
+  //Get device observations
+  temperature = carrier.Env.readTemperature(CELSIUS);
+  humidity = carrier.Env.readHumidity();
+  int raw_moisture = analogRead(moistPin);
+  moisture = map(raw_moisture, 0, 1023, 0, 100);
+
   
   // Correct JSON format
-  const char * message = "{\"device_name\":\"device_001\",\"data\":\"this is a dummy message\"}";
-  
+  String message = "{\"device_name\":\"device_001\",\"data\":{\"temperature\":";
+  message += temperature;
+  message += ",\"humidity\":";
+  message += humidity;
+  message += ",\"moisture\":";
+  message += moisture;
+  message += "}}";
+
+
+  //convert message to char pointer
+  const char * message_ = message.c_str();
+
+  //check if connection is still up
+  if (!client.connected()) {
+    reconnectToMQTTClient();
+  }
+
   // Check if publish is successful
-  if (client.publish("d2c.device_001",message)) {
+  if (client.publish("d2c.device_001",message_)) {
     Serial.println("Message published successfully");
-    carrier.display.fillScreen(0xFC00);
+    Serial.println(message_);
+    carrier.display.fillScreen(0x07E0);
     carrier.display.print("Message published successfully");
   } else {
     Serial.println("Message publish failed");
-    carrier.display.fillScreen(0xFC00);
+    carrier.display.fillScreen(0xF800);
     carrier.display.print("Message publish failed");
   }
 
-  delay(5000);
+  delay(10000);
 }
