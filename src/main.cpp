cd39f6257e1cbdb9621b873142a0b63354b3bfa2
Luis Trinta <33879012+luistrinta@users.noreply.github.com>
2024-08-25 15:54:31 +0100

    Update device code use cases
    The device connection method was updated to use a connection string field.
    Added parsing function for the connection string.
    
    In order to user this device a connection string must be provided in the config.h file.

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@ float temperature = 0;
 float humidity = 0;
 float moisture = 0;
 const int moistPin = A6;
+String BROKER, PORT_NUMBER, device_id, key;
 
 void resetScreen()
 {
@@ -26,7 +27,7 @@ void connectWiFi(){
     Serial.println("Connecting to WiFi...");
     carrier.display.fillScreen(0xFC00);
     carrier.display.print("Connecting to WiFi...");   
-    WiFi.begin(SECRET_SSID,SECRET_PASS);
+    WiFi.begin(WIFI_NAME,WIFI_PASSWORD);
     delay(500);
   }
 
@@ -46,7 +47,7 @@ void reconnectToMQTTClient()
     carrier.display.fillScreen(0xFC00);
     carrier.display.print("Attempting to reconnect to MQTTClient");
     
-    if(client.connect(CLIENT_NAME))
+    if(client.connect(device_id.c_str()))
     {
       Serial.println("Connected");
       carrier.display.fillScreen(0x07E0);
@@ -67,19 +68,66 @@ void reconnectToMQTTClient()
   }
 }
 
-void createMQTTClient()
-{
+void parseConnectionString(const String& connectionString, 
+                           String& BROKER, 
+                           String& PORT_NUMBER, 
+                           String& device_id, 
+                           String& key) {
+    int start = 0;
+    int pos = 0;
+    
+    // Parse HostName
+    pos = connectionString.indexOf("HostName=", start);
+    if (pos != -1) {
+        start = pos + 9;  // Length of "HostName="
+        pos = connectionString.indexOf(':', start);
+        if (pos != -1) {
+            BROKER = connectionString.substring(start, pos);
+            start = pos + 1;
+            pos = connectionString.indexOf(';', start);
+            PORT_NUMBER = connectionString.substring(start, pos);
+            start = pos + 1;
+        }
+    }
+
+    // Parse DeviceID
+    pos = connectionString.indexOf("DeviceID=", start);
+    if (pos != -1) {
+        start = pos + 9;  // Length of "DeviceID="
+        pos = connectionString.indexOf(';', start);
+        device_id = connectionString.substring(start, pos);
+        start = pos + 1;
+    }
+
+    // Parse Key
+    pos = connectionString.indexOf("Key=", start);
+    if (pos != -1) {
+        start = pos + 4;  // Length of "Key="
+        key = connectionString.substring(start);
+    }
+}
+
+void createMQTTClient(const char * BROKER,
+                      uint16_t PORT_NUMBER){
+
   client.setServer(BROKER,PORT_NUMBER);
   reconnectToMQTTClient();
   client.setKeepAlive(30);
+
 }
 
 void setup() {
   Serial.begin(9600);
   CARRIER_CASE = true;
   carrier.begin();
+
+  parseConnectionString(CONNECTION_STRING, BROKER, PORT_NUMBER, device_id, key);
   connectWiFi();
-  createMQTTClient();
+
+  Serial.println(BROKER);
+  Serial.println(PORT_NUMBER);
+  
+  createMQTTClient(BROKER.c_str(),PORT_NUMBER.toInt());
   resetScreen();
 }
 
@@ -89,14 +137,18 @@ void loop() {
   client.loop();
 
   //Get device observations
-  temperature = carrier.Env.readTemperature(CELSIUS);
+  temperature = carrier.Env.readTemperature(CELSIUS) -5;
   humidity = carrier.Env.readHumidity();
   int raw_moisture = analogRead(moistPin);
   moisture = map(raw_moisture, 0, 1023, 0, 100);
 
   
   // Correct JSON format
-  String message = "{\"device_name\":\"device_001\",\"data\":{\"temperature\":";
+  String message = "{\"device_id\":\"";
+  message+=device_id;
+  message+="\",\"key\":\"";
+  message+=key;
+  message+="\",\"data\":{\"temperature\":";
   message += temperature;
   message += ",\"humidity\":";
   message += humidity;
